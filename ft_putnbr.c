ed59b99825c8409052feb29ac58a2b76c7b2e3bd
Asmets <asmets@student.42.fr>
2015-01-06 17:25:56 +0100

    ft_putnbr(fd) ok

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,23 +6,25 @@
 /*   By: asmets <asmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/09 16:52:32 by asmets            #+#    #+#             */
-/*   Updated: 2015/01/06 15:54:11 by asmets           ###   ########.fr       */
+/*   Updated: 2015/01/06 17:15:42 by asmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 void	ft_putnbr(int nb)
 {
-	unsigned int nbr;
+	if(nb == -2147483648)
+	{
+		ft_putstr("-2147483648");	
+		return ;
+	}
 	if (nb < 0)
 	{
 		ft_putchar('-');
-		nbr = -nb;
+		nb = -nb;
 	}
-	else
-		nbr = nb;
-	if (nbr > 10)
-		ft_putnbr(nbr / 10);
-	ft_putchar((nbr % 10) + '0');
+	if (nb /10 != 0)
+		ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + '0');
 }
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: asmets <asmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/09 16:52:32 by asmets            #+#    #+#             */
-/*   Updated: 2014/11/13 19:53:05 by asmets           ###   ########.fr       */
+/*   Updated: 2015/01/06 17:23:37 by asmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 void	ft_putnbr_fd(int nb, int fd)
 {
-	unsigned int nbr;
-
+	if (nb == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return;
+	}
 	if (nb < 0)
 	{
 		ft_putchar_fd('-', fd);
-		nbr = -nb;
+		nb = -nb;
 	}
-	else
-		nbr = nb;
-	if (nbr > 10)
-		ft_putnbr_fd((nbr / 10), fd);
-	ft_putchar_fd(((nbr % 10) + '0'), fd);
+	
+	if (nb /10 != 0)
+		ft_putnbr_fd((nb / 10), fd);
+	ft_putchar_fd(((nb % 10) + '0'), fd);
 }
